1154a1695101aa0d09b34ef8323198f60411f9a7
josemanuel <menendezpoo@gmail.com>
2016-10-13 17:31:57 -0500

    Branching Test

---
+++ latte/fragment.store/ts/OpenPayCCView.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by josemanuel on 10/12/16.
+ */
+module latte {
+
+    /**
+     *
+     */
+    export class OpenPayCCView extends OpenPayCCViewBase {
+
+        //region Static
+        //endregion
+
+        //region Fields
+        //endregion
+
+        /**
+         *
+         */
+        constructor() {
+            super();
+        }
+
+        //region Private Methods
+        //endregion
+
+        //region Methods
+        //endregion
+
+        //region Events
+        //endregion
+
+        //region Properties
+        //endregion
+
+    }
+
+}
